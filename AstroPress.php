87cecb53cccaa5d6a1610ca2d7a30b956eabbdfb
aheadzen <aheadzen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-05 11:58:25 +0000

    uploading final plugin 1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/astropress-by-ask-oracle/trunk@1020205 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ AstroPress.php
@@ -0,0 +1,104 @@
+<?php
+/*
+Plugin Name: AstroPress by Ask Oracle
+Plugin URI: http://www.aheadzen.com
+Description: Hands-down, easiest way to embed horoscopes and astrology charts on your blog or website, we got a plugin for you!. New widget - "AstroPress Widget" added and short code eg.- "[astropress width=500 height=600]"
+Version: 1.0.0.0
+Author: Ask Oracle Team
+Author URI: http://ask-oracle.com/
+
+Copyright: © 2014-2015 ASK-ORACLE.COM
+License: GNU General Public License v3.0
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+/*******************************
+Plugin Init Function
+****************************/
+add_action('init','astropress_init');
+function astropress_init()
+{
+}
+
+/**Widget Initialize**/
+add_action( 'widgets_init','widget_astropress_init');
+
+/*******************************
+Widget Init Function
+****************************/
+function widget_astropress_init()
+{
+	register_widget('astropress_widget');
+}
+
+/*******************************
+Widget Function
+****************************/
+
+if(!class_exists('astropress_widget')){
+	class astropress_widget extends WP_Widget {
+		function astropress_widget() {
+		//Constructor
+			$widget_ops = array('classname' => 'widget astropress_widget', 'description' => 'AstroPress Widget to embed horoscopes' );		
+			$this->WP_Widget('astropress','AstroPress Widget', $widget_ops);
+		}
+		function widget($args, $instance) {
+		// prints the widget
+			extract($args, EXTR_SKIP);
+			$title = empty($instance['title']) ? '' : apply_filters('widget_title', $instance['title']);
+			$w = empty($instance['w']) ? '600' : apply_filters('widget_w', intval($instance['w']));
+			$h = empty($instance['h']) ? '600' : apply_filters('widget_h', intval($instance['h']));
+			if(!$w){$w=600;}
+			if(!$h){$h=600;}
+			echo $before_widget;
+			if($title){ echo $before_title.$title.$after_title; }
+			echo astropress_iframe_code($w,$h);
+			echo $after_widget;				
+		}
+			
+		function update($new_instance, $old_instance) {
+		//save the widget
+			$instance = $new_instance;		
+			return $instance;
+		}
+		
+		function form($instance) {
+		//widgetform in backend
+			$instance = wp_parse_args( (array) $instance, array( 'title' => '', 'w' => '600', 'h' => '600') );		
+			$title = strip_tags($instance['title']);
+			$w = strip_tags($instance['w']);
+			$h = ($instance['h']);			
+			?>
+			<p><label for="<?php  echo $this->get_field_id('title'); ?>"><?php _e('Widget Title','aheadzen');?>: <input class="widefat" id="<?php  echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></label>
+			</p>  
+			<p><label for="<?php  echo $this->get_field_id('w'); ?>"><?php _e('Display Width in pixels','aheadzen');?>: <input class="widefat" id="<?php  echo $this->get_field_id('w'); ?>" name="<?php echo $this->get_field_name('w'); ?>" type="text" value="<?php echo esc_attr($w); ?>" /></label>
+			<small><?php _e('eg: default & maximum display screen size : 600 (in pixcels)','aheadzen');?></small>
+			</p>     
+			<p><label for="<?php echo $this->get_field_id('h'); ?>"><?php _e('Display Height in pixels','aheadzen');?>: 
+			<input class="widefat" id="<?php  echo $this->get_field_id('h'); ?>" name="<?php echo $this->get_field_name('h'); ?>" type="text" value="<?php echo esc_attr($h); ?>" /></label>
+			<small><?php _e('eg: default : 600, you can adjust as per you want.','aheadzen');?></small>
+			</p>
+			<?php
+		}
+	}
+}
+
+
+/*******************************
+shotcode :: [astropress]
+****************************/
+function astropress_shortcode($atts) {
+	$atts['shortcode']=1;
+	$w = intval($atts['width']);
+	$h = intval($atts['height']);
+	if(!$w){$w=650;}
+	if(!$h){$h=750;}
+	$content = astropress_iframe_code($w,$h);
+	return $content;
+}
+add_shortcode('astropress', 'astropress_shortcode');
+
+function astropress_iframe_code($w=600,$h=600)
+{
+	return '<iframe src="http://www.ask-oracle.com/embed/" style="max-width:'.$w.'px;" width="'.$w.'px" height="'. $h.'px" frameborder="0" ></iframe>';
+}
---
+++ readme.txt
@@ -0,0 +1,59 @@
+=== AstroPress by Ask Oracle ===
+Contributors: aheadzen
+Tags: horoscope, astrology, embed, daily, weekly, monthly, love
+Requires at least : 3.0.0
+Tested up to: 4.0
+Stable tag: 1.0.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+
+Hands-down, easiest way to embed horoscopes and astrology charts on your blog or website, we got a plugin for you!
+
+
+== Description ==
+
+If you want the hands-down, easiest way to embed horoscopes and astrology charts on your blog or website, we got a plugin for you!
+Horoscope and Compatibility readings are a perfect way to learn and understand more about your relationships, day-to-day life and know more about anything that is close to your heart and holds any importance to you. Our readings are more realistic, positive and offers useful advise to enjoy the best of all there is.
+Features :
+• Daily, Weekly and Monthly Horoscopes
+• Overview, Love and Money Horoscopes
+• Receive horoscope updates and notifications
+• Email us your queries/feedback
+Instructions to use as a widget - "AstroPress Widget"
+Instructions to use short code - [astropress width=500 height=600]
+
+
+Visit [http://aheadzen.com/?p=5344]
+
+== Installation ==
+1. Unzip and upload plugin folder to your /wp-content/plugins/ directory  OR Go to wp-admin > plugins > Add new Plugin & Upload plugin zip.
+2. Go to wp-admin > Plugins(left menu) > Activate the plugin
+3. See the new widget("AstroPress Widget") added in your wp-admin > widget section OR 
+also can use shortcode  [astropress width=500 height=600]
+where 
+width = width of display screen in pixels, maximum screen width is 600px only and minimum width is 300px
+height = height of display screen in pixels, you can adjust as per you want.
+
+== Screenshots ==
+1. Main Zodiacs Screen
+2. Zodiac Daily Overview Horoscopes
+3. Zodiac Daily Career Horoscopes
+4. Zodiac Daily Love Horoscopes
+5. Zodiac Weekly Overview, Love & Career Horoscopes
+6. Zodiac Monthly Overview, Love & Career Horoscopes
+7. AstroPress Widget
+8. AstroPress Shortcode
+
+== Configuration ==
+
+1. Go to wp-admin >  widgets > get the new widget added named "AstroPress Widget" > drag & drop  as per your widget display area.
+2. Get the shortcode :: [astropress width=500 height=600] ==> add the shortcode by create new page or post of your wordpress site.
+3. If you cannot see widget or either shotcode not working? make sure you have installed & active the plugin.
+
+== Changelog ==
+
+= 1.0.0.0 =
+* Fresh Public Release.
+
+
